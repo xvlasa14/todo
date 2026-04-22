bb5f20d2c98be048518e5ca10ce7373a7daef1c9
Nela Vlasakova <nel.vlasakova@gmail.com>
2026-04-22 18:12:29 +0200

    modal, add new task

--- app/app.vue
+++ app/app.vue
@@ -3,7 +3,12 @@ import { ref, computed } from "vue";
 import tasksData from "../public/data/tasks.json";
 
 const data = ref(tasksData);
+
 const plannedOnly = ref(false);
+
+const isModalOpen = ref(false);
+const newTaskTitle = ref('')
+const newTaskPriority = ref('normal')
 
 const tasks = computed(() => {
   let result = [...data.value];
@@ -38,6 +43,22 @@ const deleteTask = (id) => {
   data.value = data.value.filter((t) => t.id !== id);
 };
 
+const addTask = () => {
+  if (!newTaskTitle.value.trim()) return
+
+  data.value.push({
+    id: Date.now(),
+    title: newTaskTitle.value,
+    status: 'planned',
+    priority: newTaskPriority.value
+  })
+
+  newTaskTitle.value = ''
+  newTaskPriority.value = 'normal'
+
+  isModalOpen.value = false
+}
+
 const summary = computed(() => {
   const tasksTotal = data.value.length;
   const tasksFinished = data.value.filter(
@@ -54,7 +75,7 @@ const summary = computed(() => {
   <div>
     <div>
       <h1>Moje úkoly</h1>
-      <button>Nový úkol</button>
+      <button @click="isModalOpen = true">Nový úkol</button>
       <label>
         <input type="checkbox" v-model="plannedOnly" />
         Pouze nedokončené
@@ -76,4 +97,38 @@ const summary = computed(() => {
     <p>Celkem úkolů: {{ summary.tasksTotal }}</p>
     <p>Dokončeno: {{ summary.tasksFinished }}</p>
   </div>
+
+<div v-if="isModalOpen" class="modal">
+  <div class="modal-content">
+    <h2>Nový úkol</h2>
+
+    <!-- název -->
+    <input 
+      v-model="newTaskTitle" 
+      placeholder="Název úkolu"
+    />
+
+    <!-- priority -->
+    <div>
+      <label>
+        <input type="radio" value="low" v-model="newTaskPriority" />
+        Nízká
+      </label>
+
+      <label>
+        <input type="radio" value="normal" v-model="newTaskPriority" />
+        Normální
+      </label>
+
+      <label>
+        <input type="radio" value="high" v-model="newTaskPriority" />
+        Vysoká
+      </label>
+    </div>
+
+    <!-- akce -->
+    <button @click="addTask">Přidat</button>
+    <button @click="isModalOpen = false">Zrušit</button>
+  </div>
+</div>
 </template>
